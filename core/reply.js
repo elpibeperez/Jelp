40b45eadd69ea96c06c7a03b2139fca4db662d9f
Hector Eduardo Perez <heperez@falabella.cl>
2022-10-18 19:12:19 -0300

    Basic review and replies logic implemented

--- core/reply.js
+++ core/reply.js
@@ -1,5 +1,23 @@
-const create_reply = (reply) => {
+const replies_model = require('../models/replies');
+const model_utils = require('../models/utils');
 
+
+const create_reply_for_review = async (replyer_id, review_id, reply) => {
+    reply.replier = replyer_id; 
+    reply.review = review_id;
+    return await replies_model.create(reply);
 }
 
-module.exports
+const create_reply_for_reply = async (replyer_id, review_id, reply_id, reply) => {
+    reply.replier = replyer_id; 
+    reply.review = review_id;
+    reply.father_reply = reply_id
+    return await replies_model.create(reply);
+}
+
+
+
+module.exports = {
+    create_reply_for_review,
+    create_reply_for_reply
+}
--- models/replies.js
+++ models/replies.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
 
-
-/*id_respuesta
-id_comentario
-id_respuesta_padre
-texto_respuesta*/
-
 const Replies = new mongoose.Schema({
-    id_comment: { type: mongoose.ObjectId, required: true},
-    id_reply: { type: mongoose.ObjectId},
+    replier: { type: mongoose.ObjectId, required: true},
+    review: { type: mongoose.ObjectId, required: true},
+    father_reply: { type: mongoose.ObjectId},
     reply: { type: String, required: true },
 });
 
